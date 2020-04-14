2685d81eae62948af09167ec6ec01d93a77665ee
Mohamed Abubucker <abubucker.sed@gmail.com>
2020-04-14 12:29:22 +0530

    WIP: merged UI into the backend API project to make it simple

--- app.js
+++ app.js
@@ -23,17 +23,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.get("/", (req, res) => {
+  res.sendFile("index.html");
+});
+
+
+app.use('/api', index);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
